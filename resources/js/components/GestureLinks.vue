c28915f44ff31651e3892d6b9628d842b5cf3194
Manuel Steiner <manuel.steiner@mts-international.org>
2021-01-04 18:51:27 +1100

    add mobile gestures overlay

--- resources/js/components/GestureLinks.vue
+++ resources/js/components/GestureLinks.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="gesture-area" ref="gestureArea" v-hammer:pan="onPan">
+    <div class="gesture-area rounded" ref="gestureArea" v-hammer:pan="onPan">
+        <div v-bind:class="panClasses" class="overlay rounded position-absolute w-100 h-100 z-10" ref="overlay">
+            <div v-bind:class="panRightClasses" class="rounded bg-blue-100 text-blue-800 align-items-center justify-content-center h-100 w-100 mr-auto">
+                <svg class="feather feather-24 mr-2">
+                    <use xlink:href="svg/feather-sprite.svg#edit"></use>
+                </svg>
+            </div>
+            <div v-bind:class="panLeftClasses" class="rounded bg-yellow-100 text-yellow-800 align-items-center justify-content-center h-100 w-100 ml-auto">
+                <svg class="feather feather-24 mr-2">
+                    <use xlink:href="svg/feather-sprite.svg#star"></use>
+                </svg>
+            </div>
+        </div>
+
         <slot></slot>
     </div>
 </template>
@@ -14,6 +27,27 @@
             }
         },
 
+        computed: {
+            panClasses: function() {
+                return {
+                    'd-none': this.totalOffset === 0,
+                    'd-flex': this.totalOffset !== 0,
+                }
+            },
+            panLeftClasses: function() {
+                return {
+                    'd-none': this.totalOffset >= 0,
+                    'd-flex': this.totalOffset < 0,
+                }
+            },
+            panRightClasses: function () {
+                return {
+                    'd-none': this.totalOffset <= 0,
+                    'd-flex': this.totalOffset > 0,
+                }
+            }
+        },
+
         methods: {
             onPan(event) {
                 const minimumOffset = 200;
@@ -23,6 +57,7 @@
                 this.totalOffset = this.totalOffset + event.deltaX/scaleFactor;
 
                 this.$refs.gestureArea.style.setProperty("--x", event.deltaX/scaleFactor);
+                this.$refs.overlay.style.setProperty("--opacity", Math.abs(event.deltaX/scaleFactor));
 
                 if(event.isFinal) {
                     this.$refs.gestureArea.style.setProperty("--x", 0);
@@ -50,4 +85,7 @@
         transform: translateX(calc(var(--x, 0) * 1%));
     }
 
+    .overlay {
+        opacity: calc(var(--opacity, 0) * 20%);
+    }
 </style>

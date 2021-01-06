a4235535d8b6d31a7a0b2a9930aeb0d882d733ce
Manuel Steiner <manuel.steiner@mts-international.org>
2021-01-06 19:13:45 +1100

    bump the application version

--- resources/views/help/changelog.blade.php
+++ resources/views/help/changelog.blade.php
@@ -11,7 +11,18 @@
 
     <div class="container my-4">
         @markdown
-        # Quokka v0.1.1- (06.01.2021)
+        # Quokka v0.1.1-855be49 (06.01.2021)
+        * Beteiligte Mitarbeiter an Aufgaben und Aktenvermerken werden automatisch beim Erstellen oder Bearbeiten
+        benachrichtigt.
+        * Beiteiligte Mitarbeiter an Aufgaben werden automatisch beim Erstellen oder Bearbeiten eines Kommentars
+        benachrichtigt.
+        * Einzelne Mitarbeiter können in Bemerkungen von Aufgaben, Kommentaren von Aufgaben, Vermerken von
+        Aktenvermerken und Kurzberichten von Serviceberichten erwähnt werden. Dies funktioniert durch tippen von
+        `@<Quokka Benutzername>` im Text. Erwähnte Benutzer werden automatisch benachrichtigt.
+        * In den Applikationseinstellungen kann ein Quokka Benutzer angegeben werden, der immer benachrichtigt wird,
+        sobald ein Bericht unterschrieben wurde. Der Techniker, welcher den Bericht verfasst hat, wird ebenfalls
+        automatisch über eine Unterschrift benachrichtigt.
+        * Diverse Verbesserungen an der Benutzeroberfläche wurden implementiert.
         @endmarkdown
     </div>
 

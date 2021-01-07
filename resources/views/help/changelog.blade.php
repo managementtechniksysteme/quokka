ef0fb901b3f953151bdd8cbcf96a5dc14d10811d
Manuel Steiner <manuel.steiner@mts-international.org>
2021-01-07 20:48:00 +1100

    bump the application version

--- resources/views/help/changelog.blade.php
+++ resources/views/help/changelog.blade.php
@@ -11,6 +11,16 @@
 
     <div class="container my-4">
         @markdown
+        # Quokka v0.1.2-5b3df57 (07.01.2021)
+        * Mit Authentisierung geschützte Sicherheitseinstellungen wurden für Benutzern hinzugefügt. Der Benutzer kann
+        sein Passowrt nun selbst ändern.
+        * Die Möglichkeit, sich mittels Zwei-Faktor-Authentisierung anzumelden, wurde implementiert. Hierzu ist eine
+        entsprechende Applikation am Smartphone notwendig. Geeignete Applikationen sind etwa die Google Authenticator
+        Applikation (https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2) oder andOTP
+        (https://play.google.com/store/apps/details?id=org.shadowice.flocke.andotp).
+        @endmarkdown
+
+        @markdown
         # Quokka v0.1.1-855be49 (06.01.2021)
         * Beteiligte Mitarbeiter an Aufgaben und Aktenvermerken werden automatisch beim Erstellen oder Bearbeiten
         benachrichtigt.

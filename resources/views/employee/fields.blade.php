e3527bea4c499273e3dfa15fbbcc4589a4ba7ee7
Manuel Steiner <manuel.steiner@mts-international.org>
2021-01-08 20:40:43 +1100

    fix employee edit icon

--- resources/views/employee/fields.blade.php
+++ resources/views/employee/fields.blade.php
@@ -17,7 +17,7 @@
     <div class="col-md-4">
         <p class="d-inline-flex align-items-center mb-1">
             <svg class="feather feather-16 mr-2">
-                <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#briefcase"></use>
+                <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#user"></use>
             </svg>
             Stammdaten
         </p>

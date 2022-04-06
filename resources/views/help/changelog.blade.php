c256ab954ebbdd8c0eddcc54b6e5307ceb28047e
Manuel Steiner <manuel.steiner@mts-international.org>
2022-04-06 15:27:26 +0200

    simplify changelog

--- resources/views/help/changelog.blade.php
+++ resources/views/help/changelog.blade.php
@@ -11,19 +11,19 @@
 
     <div class="container my-4">
         @markdown
-        ### Quokka v0.1.5-b85c62a (29.01.2021)
+        ### v0.1.5-b85c62a (29.01.2021)
         * Anhänge von Aktenvermerke und Serviceberichten werden per Email mitversendet, falls ausgewählt. Per Default
         sind alle Anhänge zum Versand vorgemerkt. Anhänge sind einzeln an- oder abwählbar.
         @endmarkdown
 
         @markdown
-        ### Quokka v0.1.4-bcc44b5 (12.01.2021)
+        ### v0.1.4-bcc44b5 (12.01.2021)
         * Neben Bilddateien sind nun auch Anhänge im PDF Format erlaubt.
         @endmarkdown
 
         @markdown
-        ### Quokka v0.1.3-7fbd636 (08.01.2021)
-        * Quokka Benutzern stehen nun verschiedene Einstellungen zum Aussehen der Applikation bereit. So kann die Anzahl
+        ### v0.1.3-7fbd636 (08.01.2021)
+        * Benutzern stehen nun verschiedene Einstellungen zum Aussehen der Applikation bereit. So kann die Anzahl
         von Listenelementen, die Sortierung von Kommentaren in Aufgaben und die Anzeige von erledigten Elementen
         angepasst werden.
         * Die Suchfunktion bei Aufgaben, Aktenvermerken und Serviceberichten wurde um sinnvolle Vorlagen erweitert. Diese
@@ -31,7 +31,7 @@
         @endmarkdown
 
         @markdown
-        ### Quokka v0.1.2-5b3df57 (07.01.2021)
+        ### v0.1.2-5b3df57 (07.01.2021)
         * Mit Authentisierung geschützte Sicherheitseinstellungen wurden für Benutzer hinzugefügt. Der Benutzer kann
         sein Passowrt nun selbst ändern.
         * Die Möglichkeit, sich mittels Zwei-Faktor-Authentisierung anzumelden, wurde implementiert. Hierzu ist eine
@@ -41,15 +41,15 @@
         @endmarkdown
 
         @markdown
-        ### Quokka v0.1.1-855be49 (06.01.2021)
+        ### v0.1.1-855be49 (06.01.2021)
         * Beteiligte Mitarbeiter an Aufgaben und Aktenvermerken werden automatisch beim Erstellen oder Bearbeiten
         benachrichtigt.
         * Beiteiligte Mitarbeiter an Aufgaben werden automatisch beim Erstellen oder Bearbeiten eines Kommentars
         benachrichtigt.
         * Einzelne Mitarbeiter können in Bemerkungen von Aufgaben, Kommentaren von Aufgaben, Vermerken von
         Aktenvermerken und Kurzberichten von Serviceberichten erwähnt werden. Dies funktioniert durch tippen von
-        `@<Quokka Benutzername>` im Text. Erwähnte Benutzer werden automatisch benachrichtigt.
-        * In den Applikationseinstellungen kann ein Quokka Benutzer angegeben werden, der immer benachrichtigt wird,
+        `@<Benutzername>` im Text. Erwähnte Benutzer werden automatisch benachrichtigt.
+        * In den Applikationseinstellungen kann ein Benutzer angegeben werden, der immer benachrichtigt wird,
         sobald ein Bericht unterschrieben wurde. Der Techniker, welcher den Bericht verfasst hat, wird ebenfalls
         automatisch über eine Unterschrift benachrichtigt.
         * Diverse Verbesserungen an der Benutzeroberfläche wurden implementiert.

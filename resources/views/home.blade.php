6db6cfd801d6acd1c057de3d976d8dd60e854814
Manuel Steiner <manuel.steiner@mts-international.org>
2022-04-27 08:24:15 +0200

    fix missing vibration duration for webpush notification about holiday allowance adjustment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="container-fluid h-100 d-flex flex-column align-items-center justify-content-center">
-        <div class="text-center">
+
+        <div class="text-center pt-4">
             <img class="empty-state" src="{{ asset('svg/dashboard.svg') }}" alt="relax" />
             <p class="lead text-muted mt-4">Hier entsteht in Kürze deine persönliche Übersicht.<br />
                 Bitte gedulde dich noch ein wenig.</p>
@@ -11,49 +12,48 @@
 
         </div>
 
-            <div class="row">
-                <div class="col col-sm-6 col-md-4 col-xl-auto mt-2">
-                    <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('companies.index') }}">
-                        <svg class="feather feather-20 mr-2">
-                            <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#briefcase"></use>
-                        </svg>
-                        Firmen anzeigen
-                    </a>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
-                    <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('projects.index') }}">
-                        <svg class="feather feather-20 mr-2">
-                            <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#clipboard"></use>
-                        </svg>
-                        Projekte anzeigen
-                    </a>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
-                    <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('tasks.index') }}">
-                        <svg class="feather feather-20 mr-2">
-                            <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#check-square"></use>
-                        </svg>
-                        Aufgaben anzeigen
-                    </a>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
-                    <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('accounting.index') }}">
-                        <svg class="feather feather-20 mr-2">
-                            <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#clock"></use>
-                        </svg>
-                        Zur Abrechnung
-                    </a>
-                </div>
-                <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
-                    <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('logbook.index') }}">
-                        <svg class="feather feather-20 mr-2">
-                            <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#book"></use>
-                        </svg>
-                        Zum Fahrtenbuch
-                    </a>
-                </div>
+        <div class="row pb-4">
+            <div class="col col-sm-6 col-md-4 col-xl-auto mt-2">
+                <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('companies.index') }}">
+                    <svg class="feather feather-20 mr-2">
+                        <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#briefcase"></use>
+                    </svg>
+                    Firmen anzeigen
+                </a>
             </div>
-
+            <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
+                <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('projects.index') }}">
+                    <svg class="feather feather-20 mr-2">
+                        <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#clipboard"></use>
+                    </svg>
+                    Projekte anzeigen
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
+                <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('tasks.index') }}">
+                    <svg class="feather feather-20 mr-2">
+                        <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#check-square"></use>
+                    </svg>
+                    Aufgaben anzeigen
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
+                <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('accounting.index') }}">
+                    <svg class="feather feather-20 mr-2">
+                        <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#clock"></use>
+                    </svg>
+                    Zur Abrechnung
+                </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-xl-auto  mt-2">
+                <a class="btn btn-primary btn-lg btn-block d-inline-flex align-items-center justify-content-center" href="{{ route('logbook.index') }}">
+                    <svg class="feather feather-20 mr-2">
+                        <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#book"></use>
+                    </svg>
+                    Zum Fahrtenbuch
+                </a>
+            </div>
+        </div>
 
     </div>
 @endsection

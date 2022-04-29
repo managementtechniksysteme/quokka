bb11905c97b5074415c727c5070bf1b8aca43cbd
Manuel Steiner <manuel.steiner@mts-international.org>
2022-04-29 23:43:54 +0200

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/employees/{employee}/access-deny', [EmployeeController::class, 'denyAccess'])->name('employees.access-deny');
     Route::get('/employees/{employee}/edit-permissions', [EmployeeController::class, 'editPermissions'])->name('employees.edit-permissions');
     Route::patch('/employees/{employee}/edit-permissions', [EmployeeController::class, 'updatePermissions'])->name('employees.update-permissions');
-    Route::post('/employees/{employee}/edit-permissions', [EmployeeController::class, 'updatePermissions'])->name('employees.update-permissions');
 
     Route::resource('help', HelpController::class)->only(['index', 'show']);
     Route::get('changelog', [ChangelogController::class, 'show'])->name('changelog.show');

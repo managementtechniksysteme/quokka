6b1ee6d0e30bc6c712ce980604d3689649142081
Manuel Steiner <manuel.steiner@mts-international.org>
2022-05-02 20:11:38 +0200

    implement kilometre rules for logbook with custom error messages

--- app/Http/Requests/LogbookStoreRequest.php
+++ app/Http/Requests/LogbookStoreRequest.php
@@ -8,17 +8,32 @@ class LogbookStoreRequest extends FormRequest
 {
     public function rules(): array
     {
+        $required_start_kilometres = $this->input('end_kilometres')-$this->input('driven_kilometres');
+        $required_end_kilometres = $this->input('start_kilometres')+$this->input('driven_kilometres');
+        $required_driven_kilometres = $this->input('end_kilometres')-$this->input('start_kilometres');
+
         return [
             'driven_on' => 'required|date',
-            'start_kilometres' => 'required|integer|min:0|lt:end_kilometres',
-            'end_kilometres' => 'required|integer|min:1|gt:start_kilometres',
-            'driven_kilometres' => 'required|integer|min:1',
+            'start_kilometres' => "required|integer|min:0|lt:end_kilometres|size:{$required_start_kilometres}",
+            'end_kilometres' => "required|integer|min:1|gt:start_kilometres|size:{$required_end_kilometres}",
+            'driven_kilometres' => "required|integer|min:1|size:{$required_driven_kilometres}",
             'litres_refuelled' => 'integer|min:1|nullable',
             'origin' => 'required',
             'destination' => 'required',
             'project_id' => 'exists:projects,id|nullable',
-            'vehicle_id' => 'exists:vehicles,id|nullable',
+            'vehicle_id' => 'exists:vehicles,id',
             'comment' => 'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'start_kilometres.lt' => 'Die Start Kilometer müssen weniger als die Ende Kilometer sein',
+            'end_kilometres.gt' => 'Die Ende Kilometer müssen mehr als die Start Kilometer sein',
+            'start_kilometres.size' => 'Die Start Kilometer müssen der Differenz aus Ende Kilometer und gefahrenen Kilometern entsprechen',
+            'end_kilometres.size' => 'Die Ende Kilometer müssen der Summe aus Start Kilometer und gefahrenen Kilometern entsprechen',
+            'driven_kilometres.size' => 'Die gefahrenen Kilometer müssen der Differenz aus Ende Kilometer und Start Kilometer entsprechen'
         ];
     }
 }

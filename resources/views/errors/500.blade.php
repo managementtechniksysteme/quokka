a18daae4d8b7091888d6ca4f1bb4cf12f4d50441
Manuel Steiner <manuel.steiner@mts-international.org>
2022-05-12 17:43:10 +0200

    add exception uuids for better support

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.minimal')
+
+@section('title', __('Server Error'))
+@section('code', '500')
+@section('message', __('Server Error'))
+@section('support-message', __('Für Hilfe gib bitte folgende Fehler Identifikation an.'))

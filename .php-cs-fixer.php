f9fd9654877f381d63cc58f197b1cff34683fc58
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-09 15:09:32 +0200

    add new php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,17 +3,20 @@
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/bootstrap/app.php';
 
-return (new MattAllan\LaravelCodeStyle\Config())
+return (new Jubeki\LaravelCodeStyle\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->in(app_path())
             ->in(config_path())
-            ->in(database_path())
-            ->notPath(database_path('migrations'))
-            ->in(resource_path('lang'))
+            ->in(database_path('factories'))
+            ->in(database_path('seeders'))
+            ->in(lang_path())
             ->in(base_path('routes'))
             ->in(base_path('tests'))
     )
     ->setRules([
         '@Laravel' => true,
-    ]);
+        // '@Laravel:risky' => true,
+    ])
+    // ->setRiskyAllowed(true)
+    ;

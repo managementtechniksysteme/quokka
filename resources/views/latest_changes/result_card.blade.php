f261286362c45802abc7e63ea74cb6cfe096a2d4
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-13 08:56:46 +0200

    fix content overflow in latest changes

--- resources/views/latest_changes/result_card.blade.php
+++ resources/views/latest_changes/result_card.blade.php
@@ -1,8 +1,8 @@
 <div class="overview-card rounded position-relative">
     <a class="stretched-link outline-none" href="{{ $result->route }}"></a>
-    <div class="d-flex w-100 p-3 align-items-center">
-        <div class="flex-grow-1 h-100">
-            <p class="m-0 mw-100 text-muted text-truncate">
+    <div class="mw-100 d-flex w-100 p-3 align-items-center">
+        <div class="mw-100 flex-grow-1 h-100">
+            <p class="mw-100 text-muted text-truncate">
                 @component('partials.model_icon', ['model' => $result->model])
                 @endcomponent
                 {{ $result->type }}

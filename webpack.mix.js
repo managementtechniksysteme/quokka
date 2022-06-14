738ac1f99cb22a2a9b10eef4d19eb4e081462a5a
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-14 11:27:23 +0200

    add browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,3 +20,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('node_modules/feather-icons/dist/feather-sprite.svg', 'public/svg')
     .copy('node_modules/bootstrap-icons/bootstrap-icons.svg', 'public/svg');
+
+mix.browserSync('127.0.0.1:8000');

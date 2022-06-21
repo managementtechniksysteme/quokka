8820aa7626fff705119bbd77f86667a9651a1ba5
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-21 17:53:04 +0200

    fix notification list view styling

--- resources/views/notification/overview_card.blade.php
+++ resources/views/notification/overview_card.blade.php
@@ -1,24 +1,24 @@
 <div class="overview-card rounded">
-    <div class="mw-100 d-flex flex-grow-1 p-3 align-items-center">
-        <div class="mw-100 flex-grow-1 h-100 position-relative">
+    <div class="d-flex flex-grow-1 p-3 align-items-center">
+        <div class="d-flex flex-column flex-grow-1 h-100 position-relative">
             <a class="stretched-link outline-none" href="{{ $notification->data['route'] }}"></a>
             <div class="text-muted">
                 @include('notification.notification_icon')
                 {{ NotificationHelper::header($notification) }}
             </div>
-            <div class="mw-100 text-truncate">
+            <div class="flex-wrap">
                 {{ NotificationHelper::text($notification) }}
             </div>
         </div>
 
-        <div class="d-none d-md-flex text-muted align-items-center mr-4">
+        <div class="d-none d-lg-flex text-muted align-items-center mr-4">
             <svg class="icon icon-16 mr-1">
                 <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#calendar"></use>
             </svg>
             {{ $notification->created_at->format('d.m.Y H:i') }}
         </div>
 
-        <div class="d-none d-md-block">
+        <div class="d-none d-lg-block">
             <form action="{{ route('notifications.destroy', $notification) }}" method="post">
                 @csrf
                 @method('DELETE')

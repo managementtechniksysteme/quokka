7cfaea0071b6cc44591a2b98ab2c996e081150d0
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-23 14:54:54 +0200

    implement better visual distinction between read and unread notifications

--- resources/views/notification/overview_card.blade.php
+++ resources/views/notification/overview_card.blade.php
@@ -2,16 +2,16 @@
     <div class="d-flex flex-grow-1 p-3 align-items-center">
         <div class="d-flex flex-column flex-grow-1 h-100 position-relative">
             <a class="stretched-link outline-none" href="{{ $notification->data['route'] }}"></a>
-            <div class="text-muted">
+            <div class="@if($notification->read_at) text-gray-500 @else text-muted @endif">
                 @include('notification.notification_icon')
                 {{ NotificationHelper::header($notification) }}
             </div>
-            <div class="flex-wrap @if($notification->read_at) text-muted @endif">
+            <div class="flex-wrap @if($notification->read_at) text-gray-500 @endif">
                 {{ NotificationHelper::text($notification) }}
             </div>
         </div>
 
-        <div class="d-none d-lg-flex flex-shrink-0 text-muted align-items-center mr-4">
+        <div class="d-none d-lg-flex flex-shrink-0 align-items-center mr-4 @if($notification->read_at) text-gray-500 @else text-muted @endif">
             <svg class="icon icon-16 mr-1">
                 <use xlink:href="{{ asset('svg/feather-sprite.svg') }}#calendar"></use>
             </svg>

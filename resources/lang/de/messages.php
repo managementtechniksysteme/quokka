3c1762cc380e8e8a0e6cb4325460bdb7bd980805
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-23 10:08:52 +0200

    update pluralisations

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -3,6 +3,7 @@
 return [
     'devices' => 'ein registriertes Gerät|:number registrierte Geräte',
     'elements' => ':count Element|:count Elemente',
+    'entries' => ':count Eintrag|:count Einträge',
     'permissions' => ':count Berechtigung|:count Berechtigungen',
     'projects' => ':count Projekt|:count Projekte',
     'results' => ':count Ergebnis|:count Ergebnisse',

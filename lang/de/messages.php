44d81a83f208ee137097a7cf7283fcb0d8fcc217
Manuel Steiner <manuel.steiner@mts-international.org>
2023-03-25 20:50:01 +0100

    Upgrade to Laravel 9

---
+++ lang/de/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'devices' => 'ein registriertes Gerät|:number registrierte Geräte',
+    'elements' => ':count Element|:count Elemente',
+    'entries' => ':count Eintrag|:count Einträge',
+    'permissions' => ':count Berechtigung|:count Berechtigungen',
+    'projects' => ':count Projekt|:count Projekte',
+    'results' => ':count Ergebnis|:count Ergebnisse',
+    'service_reports' => ':count Servicebericht|:count Serviceberichte',
+    'tasks' => ':count Aufgabe|:count Aufgaben'
+];

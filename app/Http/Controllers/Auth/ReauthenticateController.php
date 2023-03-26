83effba5bb72f02bb303f3c1cc01ed78b7555bbd
Manuel Steiner <manuel.steiner@mts-international.org>
2023-03-26 14:38:38 +0200

    upgrade to Laravel 10

--- app/Http/Controllers/Auth/ReauthenticateController.php
+++ app/Http/Controllers/Auth/ReauthenticateController.php
@@ -19,7 +19,7 @@ class ReauthenticateController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'auth.session']);
     }
 
     public function index()

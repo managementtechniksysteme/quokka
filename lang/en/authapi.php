a340b9f80b19cd87c8bb7b487dfb8ac523cde77f
Manuel Steiner <manuel.steiner@mts-international.org>
2023-04-07 21:35:30 +0200

    implement api auth flow

---
+++ lang/en/authapi.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'refresh_device_mismatch' => 'The provided device name does not match the device name on record for the provided token.',
+
+];

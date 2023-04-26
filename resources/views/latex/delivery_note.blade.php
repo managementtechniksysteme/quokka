e3af5da7df821ad342fe3d68a9d05741042fd37a
Manuel Steiner <manuel.steiner@mts-international.org>
2023-04-26 13:02:09 +0200

    fix delivery report issues

--- resources/views/latex/delivery_note.blade.php
+++ resources/views/latex/delivery_note.blade.php
@@ -6,7 +6,8 @@
 \usepackage{lastpage}
 
 \begin{document}
-\sbox\additions{
+\newsavebox{\additions}
+\sbox{\additions}{
 \begin{tikzpicture}[overlay,remember picture,shift=(current page.south west)]
 \node[anchor=south] at (5.25cm, 5cm) {{{ $deliveryNote->signature()->created_at }}};
 \end{tikzpicture}
@@ -14,5 +15,5 @@
 \node[anchor=south] at (-5.25cm, 5cm) {\includegraphics[height=2cm]{{ $deliveryNote->signature()->getPath() }}};
 \end{tikzpicture}
 }
-\includepdf[pages={1-}, pagecommand={\thispagestyle{empty}\if\thepage\pageref{LastPage}\usebox\additions\fi}]{{ $deliveryNote->document()->getPath() }}}
+\includepdf[pages={1-}, pagecommand={\thispagestyle{empty}\if\thepage\pageref{LastPage}\usebox\additions\fi}]{{{ $deliveryNote->document()->getPath() }}}
 \end{document}
